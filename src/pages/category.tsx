a39b987ec009e3c8f5b1b9f6dd1d693652b7b644
Davor Harni <harnidavor7@gmail.com>
2024-12-15 21:07:31 +0100

    e

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -5,29 +5,33 @@ import Grid from "../components/grid";
 import Card from "../components/cards/card";
 import { useGetEverythingQuery } from "../services/news";
 import LoadingSpinner from "../components/spinner";
+import { useSelector } from "react-redux";
+import { AppState } from "../store";
 import { Article } from "../models/article";
-
-
 
 const Category: React.FC = () => {
   const { category } = useParams<{ category: string }>();
   const [articles, setArticles] = useState<Article[]>([]);
   const [page, setPage] = useState(1);
 
+  const searchQuery = useSelector((state: AppState) => state.search.query);
+
   const { data: news, isLoading } = useGetEverythingQuery({
-    keyword: category || "technology",
+    keyword: searchQuery || category || "technology",
     page,
   });
 
   useEffect(() => {
     setPage(1);
     setArticles([]);
-  }, [category]);
+  }, [category, searchQuery]);
 
   useEffect(() => {
     if (news?.articles) {
       const filteredArticles = news.articles.filter(
-        (article: Article) => article.content !== "[Removed]" && article.description !== "[Removed]"
+        (article: Article) =>
+          article.content !== "[Removed]" &&
+          article.description !== "[Removed]"
       );
 
       setArticles((prevArticles) =>
@@ -49,24 +53,25 @@ const Category: React.FC = () => {
         loader={<LoadingSpinner />}
       >
         <Grid>
-        {articles.map((article: Article, index: number) => {
-          const showAdLabel = (index + 1) % 6 === 0;
+          {articles.length === 0 && !isLoading && (
+            <p>No articles found for "{searchQuery || category}".</p>
+          )}
+          {articles.map((article: Article, index: number) => {
+            const showAdLabel = (index + 1) % 6 === 0;
 
-          return (
-            <Card
-              key={index}
-              image={article.urlToImage}
-              title={article.title}
-              author={article.author}
-              url={article.url}
-              category={category}
-              showAdLabel={showAdLabel}
-            />
-          );
-        })}
-
+            return (
+              <Card
+                key={index}
+                image={article.urlToImage}
+                title={article.title}
+                author={article.author}
+                url={article.url}
+                category={category}
+                showAdLabel={showAdLabel}
+              />
+            );
+          })}
         </Grid>
-
       </InfiniteScroll>
     </>
   );
